e454123744c89ab8ddec855192f3b255be44af1b
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-28 23:13:03 +0200

    видаляти рядки з таблиць бази даних;

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # Для бази даних Академія, яку ви розробили в рамках
 # курсу «Теорія Баз Даних», створіть додаток для взаємодії
 # з базою даних, який дозволяє:
-# ■ оновлювати рядків у таблицях бази даних;
+# ■ видаляти рядки з таблиць бази даних;
 
 import json
-from sqlalchemy import create_engine, MetaData, Table, update
+from sqlalchemy import create_engine, MetaData, Table, delete
 
 # Зчитування конфігураційних даних з файлу
 with open('config.json') as f:
@@ -25,37 +25,36 @@ conn = engine.connect()
 faculties_table = Table('faculties', MetaData(), autoload_with=engine)
 
 def print_columns(columns):
-    print("Доступні колонки для оновлення: ")
+    print("Доступні колонки для видалення: ")
     for idx, column in enumerate(columns, start=1):
         print(f"{idx}.{column}")
 
-def update_rows(table):
+def delete_rows(table):
     columns = table.columns.keys()
     print_columns(columns)
 
-    selected_column_idx = int(input("Введіть номер колонки для оновлення: "))
+    selected_column_idx = int(input("Введіть номер колонки для умови видалення: "))
 
     if 1 <= selected_column_idx <= len(columns):
         condition_column = columns[selected_column_idx - 1]
     else:
-        print("Невірний номер колонки!")
+        print("Невірний номер колонки! Видалення відмінено!")
+        return
 
     condition_value = input(f"Введіть значення для умови, {condition_column}: ")
-    new_values = {}
 
-    # Введення нових значень для кожної колонки
-    for column in columns:
-        value = input(f"Введіть нове значення для колонки {column}: ")
-        new_values[column] = value
-
-    confirm_update = input("Оновити усі рядки? Так/Ні? ")
+    confirm_update = input("Видалити усі рядки з цієї таблиці? Так/Ні? ")
     if confirm_update.lower() == 'так':
-        query = update(table).where(getattr(table.c, condition_column) == condition_value).values(new_values)
+        query = delete(table).where(getattr(table.c, condition_column) == condition_value)
         conn.execute(query)
         conn.commit()
+        print("Рядки успішно видалені.")
+    else:
+        print("Видалення відмінено.")
 
-# Виклик функції для оновлення рядків у таблиці faculties
-update_rows(faculties_table)
+# Виклик функції для видалення рядків у таблиці faculties
+delete_rows(faculties_table)
 
 # Закриття з'єднання
 conn.close()
+
